a90c25a15ea8a675a9a4ca791749db9e5264f480
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-03-29 13:45:14 -0300

    Update 1008.cpp

--- 1008.cpp
+++ 1008.cpp
@@ -6,9 +6,9 @@ int main () {
 	float id, h, p, s;
 	id = rand() % 2344;
 	h = 12; //h a day
-	p = 713.71; //yen a hour
+	p = 9103.71; //yen a hour
 	s = h*p;
 	cout << "ID = " << id << endl;
-	cout << "Salary = " << setprecision (6) << s << " yen" ;
+	cout << "Salary = " << setprecision (8) << s << " yen" ;
 	
 }
