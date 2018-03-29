451a5218073ae414eb565dbffb9444811f3eacce
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-03-29 14:01:07 -0300

    Create 1009.c

---
+++ 1009.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string>
+int main () {
+	
+	char name [10];
+	printf ("Worker's name : ");
+	scanf (" %s", name);
+	printf ("\n");
+	float sells, com, fixed, new_salary;
+	sells = 15684; //in dollars
+	com = ((sells)/100)*15;
+	fixed = 2500;
+	new_salary = fixed + com;
+	printf ("Salary = $%0.2f", new_salary);
+
+}
