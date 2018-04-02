eeec2f7e4af22e8e49ae94e318428776fdad63b2
Vinícius dos Santos Chab <36959048+ViniChab@users.noreply.github.com>
2018-04-02 15:47:19 -0300

    Update and rename 1002.cpp to 1002.c

---
+++ 1002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+	
+	double R,PI,a;
+	PI = 3.14159;
+	scanf("%lf", &R);
+	a = PI * (pow(R,2));
+	printf ( "A=%0.4f", a, "\n" );
+	return 0;
+	
+}
